85c4a148b77b7c43f51bd1ffe777576f6b5f78b4
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-10-19 17:55:47 +0200

    Trying to cope with missing multi-word tokens automatically.

--- merge_raw_with_conllu.pl
+++ merge_raw_with_conllu.pl
@@ -85,6 +85,8 @@ while(<CONLLU>)
     {
         push(@sentence_start_line_numbers, $il);
     }
+    ###!!! Test: skip multi-word-token lines.
+    next if(m/^\d+-\d+/);
     # Remove sentence-terminating characters.
     s/\r?\n$//;
     push(@current_sentence, $_);
@@ -99,145 +101,166 @@ while(<CONLLU>)
 close(CONLLU);
 my $o = scalar(@conllu);
 print STDERR ("Found $o sentences in the CoNLL-U file.\n");
-# Synchronize the CoNLL-U sentences with the raw fragments.
-# According to Alessio, some sentences may be omitted in the CoNLL-U file.
-# However, all CoNLL-U sentences can be located somewhere in the raw fragments.
-my $ifrg = 0;
-my $isnt = 0;
-# Remember if the last message was "not found". Avoid displaying too many such messages.
-my $lmnf = 0;
-while($isnt <= $#conllu)
+do
 {
-    # Get the non-whitespace string of the sentence.
-    my $nwhsp = '';
-    my $from;
-    my $to;
-    foreach my $line (@{$conllu[$isnt]})
+    my $dying = 0;
+    my $restart = 0;
+    # Synchronize the CoNLL-U sentences with the raw fragments.
+    # According to Alessio, some sentences may be omitted in the CoNLL-U file.
+    # However, all CoNLL-U sentences can be located somewhere in the raw fragments.
+    my $ifrg = 0;
+    my $isnt = 0;
+    # Remember if the last message was "not found". Avoid displaying too many such messages.
+    my $lmnf = 0;
+    while($isnt <= $#conllu)
     {
-        # Process multi-word tokens.
-        if($line =~ m/^(\d+)-(\d+)\t/)
+        # Get the non-whitespace string of the sentence.
+        my $nwhsp = '';
+        my $from;
+        my $to;
+        foreach my $line (@{$conllu[$isnt]})
         {
-            $from = $1;
-            $to = $2;
-            my @f = split(/\t/, $line);
-            $nwhsp .= $f[1];
-        }
-        if($line =~ m/^(\d+)\t/)
-        {
-            my $id = $1;
-            if(defined($to) && $id > $to)
+            # Process multi-word tokens.
+            if($line =~ m/^(\d+)-(\d+)\t/)
             {
-                $from = undef;
-                $to = undef;
-            }
-            unless(defined($to))
-            {
+                $from = $1;
+                $to = $2;
                 my @f = split(/\t/, $line);
-                # For some reason, semicolons are enclosed in quotation marks in the CoNLL-U file, although they were not in original.
-                $f[1] = ';' if($f[1] eq '";"');
                 $nwhsp .= $f[1];
             }
-        }
-    }
-    $nwhsp =~ s/\s//g;
-    # Look at the beginning of the current fragment. Is the sentence there?
-    my $frgnwhsp = $fragments[$ifrg]{text};
-    $frgnwhsp =~ s/\s//g;
-    # Occasionally the CoNLL-U file does not keep the original casing, so we should compare the characters case-insensitively.
-    $nwhsp = lc($nwhsp);
-    $frgnwhsp = lc($frgnwhsp);
-    # The easiest case: the current fragment consists just of the current sentence.
-    if($nwhsp eq $frgnwhsp)
-    {
-        $metasnt[$isnt]{ifrg} = $ifrg;
-        $last_sentence_found = $metasnt[$isnt]{text} = $fragments[$ifrg]{text};
-        print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) matches fragment $ifrg (line $fragments[$ifrg]{line}).\n");
-        $lmnf = 0;
-        # Proceed to the next CoNLL-U sentence and the next fragment.
-        $isnt++;
-        $ifrg++;
-    }
-    elsif(length($nwhsp) <= length($frgnwhsp))
-    {
-        if(substr($frgnwhsp, 0, length($nwhsp)) eq $nwhsp)
-        {
-            # The current fragment begins with the current sentence.
-            # But we do not know how many extra whitespace characters there are.
-            my @frgchars = split(//, $fragments[$ifrg]{text});
-            my @sntchars = split(//, $nwhsp);
-            my $sentence = '';
-            while(scalar(@sntchars) > 0)
+            if($line =~ m/^(\d+)\t/)
             {
-                if(lc($frgchars[0]) eq lc($sntchars[0]))
+                my $id = $1;
+                if(defined($to) && $id > $to)
                 {
-                    $sentence .= shift(@frgchars);
-                    shift(@sntchars);
+                    $from = undef;
+                    $to = undef;
                 }
-                elsif($frgchars[0] =~ m/\s/)
+                unless(defined($to))
                 {
-                    $sentence .= shift(@frgchars);
-                }
-                else
-                {
-                    print STDERR ("Something is wrong!\n");
-                    print STDERR ("  Fragment remainder = '", join('', @frgchars), "'\n");
-                    print STDERR ("  Sentence remainder = '", join('', @sntchars), "'\n");
-                    die();
+                    my @f = split(/\t/, $line);
+                    # For some reason, semicolons are enclosed in quotation marks in the CoNLL-U file, although they were not in original.
+                    $f[1] = ';' if($f[1] eq '";"');
+                    $nwhsp .= $f[1];
                 }
             }
+        }
+        $nwhsp =~ s/\s//g;
+        # Look at the beginning of the current fragment. Is the sentence there?
+        my $frgnwhsp = $fragments[$ifrg]{text};
+        $frgnwhsp =~ s/\s//g;
+        # Occasionally the CoNLL-U file does not keep the original casing, so we should compare the characters case-insensitively.
+        $nwhsp = lc($nwhsp);
+        $frgnwhsp = lc($frgnwhsp);
+        # The easiest case: the current fragment consists just of the current sentence.
+        if($nwhsp eq $frgnwhsp)
+        {
             $metasnt[$isnt]{ifrg} = $ifrg;
-            $last_sentence_found = $metasnt[$isnt]{text} = $sentence;
-            # Remove the sentence from the current fragment.
-            while(length(@frgchars) > 0 && $frgchars[0] =~ m/\s/)
-            {
-                shift(@frgchars);
-            }
-            $fragments[$ifrg]{text} = join('', @frgchars);
-            print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) is a prefix of fragment $ifrg (line $fragments[$ifrg]{line}).\n");
+            $last_sentence_found = $metasnt[$isnt]{text} = $fragments[$ifrg]{text};
+            print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) matches fragment $ifrg (line $fragments[$ifrg]{line}).\n");
             $lmnf = 0;
-            # Proceed to the next CoNLL-U sentence.
+            # Proceed to the next CoNLL-U sentence and the next fragment.
             $isnt++;
+            $ifrg++;
         }
-        # The current fragment does not begin with the current sentence.
-        # Does it at least contain the current sentence?
-        elsif($frgnwhsp =~ s/^(.*?)(\Q$nwhsp\E.*)$/$2/)
+        elsif(length($nwhsp) <= length($frgnwhsp))
         {
-            # Discard the unrecognized initial part of the fragment (including whitespace).
-            my @dischars = split(//, $1);
-            my @frgchars = split(//, $fragments[$ifrg]{text});
-            while(scalar(@dischars) > 0)
+            if(substr($frgnwhsp, 0, length($nwhsp)) eq $nwhsp)
             {
-                if(lc($frgchars[0]) eq lc($dischars[0]))
+                # The current fragment begins with the current sentence.
+                # But we do not know how many extra whitespace characters there are.
+                my @frgchars = split(//, $fragments[$ifrg]{text});
+                my @sntchars = split(//, $nwhsp);
+                my $sentence = '';
+                while(scalar(@sntchars) > 0)
+                {
+                    if(lc($frgchars[0]) eq lc($sntchars[0]))
+                    {
+                        $sentence .= shift(@frgchars);
+                        shift(@sntchars);
+                    }
+                    elsif($frgchars[0] =~ m/\s/)
+                    {
+                        $sentence .= shift(@frgchars);
+                    }
+                    else
+                    {
+                        print STDERR ("Something is wrong!\n");
+                        print STDERR ("  Fragment remainder = '", join('', @frgchars), "'\n");
+                        print STDERR ("  Sentence remainder = '", join('', @sntchars), "'\n");
+                        die();
+                    }
+                }
+                $metasnt[$isnt]{ifrg} = $ifrg;
+                $last_sentence_found = $metasnt[$isnt]{text} = $sentence;
+                # Remove the sentence from the current fragment.
+                while(length(@frgchars) > 0 && $frgchars[0] =~ m/\s/)
                 {
                     shift(@frgchars);
-                    shift(@dischars);
                 }
-                elsif($frgchars[0] =~ m/\s/)
+                $fragments[$ifrg]{text} = join('', @frgchars);
+                print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) is a prefix of fragment $ifrg (line $fragments[$ifrg]{line}).\n");
+                $lmnf = 0;
+                # Proceed to the next CoNLL-U sentence.
+                $isnt++;
+            }
+            # The current fragment does not begin with the current sentence.
+            # Does it at least contain the current sentence?
+            elsif($frgnwhsp =~ s/^(.*?)(\Q$nwhsp\E.*)$/$2/)
+            {
+                # Discard the unrecognized initial part of the fragment (including whitespace).
+                my @dischars = split(//, $1);
+                my @frgchars = split(//, $fragments[$ifrg]{text});
+                while(scalar(@dischars) > 0)
+                {
+                    if(lc($frgchars[0]) eq lc($dischars[0]))
+                    {
+                        shift(@frgchars);
+                        shift(@dischars);
+                    }
+                    elsif($frgchars[0] =~ m/\s/)
+                    {
+                        shift(@frgchars);
+                    }
+                    else
+                    {
+                        print STDERR ("Something is wrong!\n");
+                        print STDERR ("  Fragment remainder = '", join('', @frgchars), "'\n");
+                        print STDERR ("  Discard remainder  = '", join('', @dischars), "'\n");
+                        die();
+                    }
+                }
+                while(scalar(@frgchars) > 0 && $frgchars[0] =~ m/\s/)
                 {
                     shift(@frgchars);
                 }
-                else
+                $fragments[$ifrg]{text} = join('', @frgchars);
+                # Now the fragment begins with the current sentence and the next pass through the loop will match them.
+                print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) found in fragment $ifrg. Discarding unmatched prefix of the fragment.\n");
+                $lmnf = 0;
+            }
+            # The current fragment does not contain the current sentence.
+            # Proceed to the next fragment.
+            else
+            {
+                print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) not found in fragment $ifrg (line $fragments[$ifrg]{line}).\n") unless($lmnf);
+                $lmnf = 1;
+                $ifrg++;
+                # If there are no more fragments, something went wrong because we were supposed to find all sentences and we didn't.
+                if($ifrg > $#fragments)
                 {
-                    print STDERR ("Something is wrong!\n");
-                    print STDERR ("  Fragment remainder = '", join('', @frgchars), "'\n");
-                    print STDERR ("  Discard remainder  = '", join('', @dischars), "'\n");
-                    die();
+                    print STDERR ("Something went wrong and we did not find the sentence $isnt (line $sentence_start_line_numbers[$isnt]):\n");
+                    print STDERR ("  '$nwhsp'\n");
+                    print STDERR ("Last sentence found:\n");
+                    print STDERR ("  '$last_sentence_found'\n");
+                    $dying = 1;
                 }
             }
-            while(scalar(@frgchars) > 0 && $frgchars[0] =~ m/\s/)
-            {
-                shift(@frgchars);
-            }
-            $fragments[$ifrg]{text} = join('', @frgchars);
-            # Now the fragment begins with the current sentence and the next pass through the loop will match them.
-            print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) found in fragment $ifrg. Discarding unmatched prefix of the fragment.\n");
-            $lmnf = 0;
         }
-        # The current fragment does not contain the current sentence.
-        # Proceed to the next fragment.
+        # Fragment is shorter than sentence. Proceed to the next fragment.
         else
         {
-            print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) not found in fragment $ifrg (line $fragments[$ifrg]{line}).\n") unless($lmnf);
+            print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) is longer than the remainder of fragment $ifrg (line $fragments[$ifrg]{line}).\n") unless($lmnf);
             $lmnf = 1;
             $ifrg++;
             # If there are no more fragments, something went wrong because we were supposed to find all sentences and we didn't.
@@ -247,27 +270,18 @@ while($isnt <= $#conllu)
                 print STDERR ("  '$nwhsp'\n");
                 print STDERR ("Last sentence found:\n");
                 print STDERR ("  '$last_sentence_found'\n");
-                die();
+                $dying = 1;
             }
         }
-    }
-    # Fragment is shorter than sentence. Proceed to the next fragment.
-    else
-    {
-        print STDERR ("Sentence $isnt (line $sentence_start_line_numbers[$isnt]) is longer than the remainder of fragment $ifrg (line $fragments[$ifrg]{line}).\n") unless($lmnf);
-        $lmnf = 1;
-        $ifrg++;
-        # If there are no more fragments, something went wrong because we were supposed to find all sentences and we didn't.
-        if($ifrg > $#fragments)
+        if($dying)
         {
-            print STDERR ("Something went wrong and we did not find the sentence $isnt (line $sentence_start_line_numbers[$isnt]):\n");
-            print STDERR ("  '$nwhsp'\n");
-            print STDERR ("Last sentence found:\n");
-            print STDERR ("  '$last_sentence_found'\n");
-            die();
+            $restart = add_mwt($conllu[$isnt]);
+            die unless($restart);
+            last;
         }
     }
 }
+while($restart);
 # All sentences have been matched against the original text. Enrich the CoNLL-U representation with metadata.
 # Friedrich Schlegel: Lyceum Fragmente
 # Novalis: Blüthenstaub
@@ -367,4 +381,56 @@ for(my $i = 0; $i <= $#conllu; $i++)
         }
         print("$line\n");
     }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Tries to add missing multi-word-token lines. There is no 100% rule for adding
+# them so this code should only be invoked when a CoNLL-U sentence cannot be
+# found in the source text.
+#------------------------------------------------------------------------------
+sub add_mwt
+{
+    my $sentence = shift;
+    my $changed_something = 0;
+    # It is possible that the input CoNLL-U file is missing multi-word-token lines.
+    # Alessio's annotation tool does not support them.
+    # We will now rely on a fixed list of preposition-article contractions and add the lines.
+    for(my $i = 1; $i <= $#{$sentence}; $i++)
+    {
+        if($sentence->[$i] =~ m/^\d+\t/ && $sentence->[$i-1] =~ m/^\d+\t/)
+        {
+            my @fc = split(/\t/, $sentence->[$i]);
+            my @fp = split(/\t/, $sentence->[$i-1]);
+            # Warning! The putative article could also be a pronoun, as in "In dem, was man Philosophie nennt..." Then it should not be contracted!
+            # More warning!!! It turns out that these text can contain uncontracted preposition+article! Quite a few occurrences like "in dem Kontrast".
+            # So we must not assume contraction if we can synchronize without it.
+            if(#$fc[3] !~ m/^(PDS|PRELS)$/ &&
+               (lc($fc[1]) eq 'dem' && lc($fp[1]) =~ m/^(in|zu|an|von)$/ ||
+                lc($fc[1]) eq 'der' && lc($fp[1]) =~ m/^(zu)$/ ||
+                lc($fc[1]) eq 'das' && lc($fp[1]) =~ m/^(in)$/))
+            {
+                if($i<2 || $sentence->[$i-2] !~ m/^\d+-\d+\t/)
+                {
+                    my @fmwt = ("$fp[0]-$fc[0]", '_', '_', '_', '_', '_', '_', '_', '_', '_');
+                    $fmwt[1] = $fp[1];
+                    my $suffix = substr($fc[1], 2, 1); # could be uppercase
+                    if($fmwt[1] =~ m/^zu$/i || $fc[1] =~ m/^(der|das)$/i)
+                    {
+                        $fmwt[1] .= $suffix;
+                    }
+                    else # im am vom
+                    {
+                        $fmwt[1] =~ s/n$/$suffix/i;
+                    }
+                    my $fmwt = join("\t", @fmwt);
+                    splice(@{$sentence}, $i-1, 0, $fmwt);
+                    $changed_something = 1;
+                    $i++;
+                }
+            }
+        }
+    }
+    return $changed_something;
 }
